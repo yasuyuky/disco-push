88742f9f70487a94c63685287d7a0ce3cd5e0815
yasuyuky <yasuyuki.ymd@gmail.com>
2022-01-08 20:14:01 +0900

    Use toml for channel configuration

--- disco-push.py
+++ disco-push.py
@@ -1,53 +1,65 @@
+import argparse
 import asyncio
 from asyncio.tasks import wait_for
 import discord
 import os
 import requests
+import toml
 
 loop = asyncio.get_event_loop()
 client = discord.Client(loop=loop)
-CHANNEL_ID = int(os.environ['CHANNEL_ID'])
+CHANNELS = {}
 PUSHOVER_URL = "https://api.pushover.net/1/messages.json"
-PUSHOVER_USER = os.environ['PUSHOVER_USER']
-PUSHOVER_TOKEN = os.environ['PUSHOVER_TOKEN']
 CH_PREFIX = os.environ.get("CH_PREFIX", "(Pushing)")
-target_channel = None
-original_name = None
 
 
 @client.event
 async def on_ready():
-    global target_channel, original_name
     for channel in client.get_all_channels():
         print(f"{channel.name} - {channel.id}")
-    target_channel = client.get_channel(CHANNEL_ID)
-    original_name = str(target_channel.name)
-    await wait_for(target_channel.edit(name=original_name + CH_PREFIX), 5.0)
+    for cid in CHANNELS:
+        c = CHANNELS[cid]
+        c["channel"] = client.get_channel(cid)
+        c["original_name"] = c["channel"].name
+    await wait_for(c["channel"].edit(name=c["channel"].name + CH_PREFIX), 5.0)
     print("READY")
 
 
 @client.event
 async def on_voice_state_update(member, before, after):
     if before.channel != after.channel:
-        if after.channel and after.channel.id == CHANNEL_ID:
+        if after.channel and after.channel.id in CHANNELS:
+            channel = CHANNELS[after.channel.id]
             print(f"{member.name} joined {after.channel.name}")
             requests.post(PUSHOVER_URL,
                           data={
-                              "token": PUSHOVER_TOKEN,
-                              "user": PUSHOVER_USER,
+                              "token": channel.pushover_token,
+                              "user": channel.pushover_user,
                               "message": f"{member.name} joined the channel."
                           })
 
 
 async def restore_channel():
-    if target_channel and original_name:
-        await wait_for(target_channel.edit(name=original_name), 5.0)
-        print("CHANNEL NAME RESTORED")
+    for cid in CHANNELS:
+        c = CHANNELS[cid]
+        if c["channel"] and c["original_name"]:
+            await wait_for(c["channel"].edit(name=c["original_name"]), 5.0)
+            print("CHANNEL NAME RESTORED")
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", help="Config file (toml)")
+    return parser
 
 
 if __name__ == '__main__':
+    parser = create_parser()
+    args = parser.parse_args()
+    config = toml.load(args.config)
+    CHANNELS = {c["channel_id"]: c for c in config["channel"]}
     try:
-        loop.run_until_complete(client.start(os.environ["DISCORD_BOT_TOKEN"]))
+        loop.run_until_complete(client.start(config["discord_bot_token"]))
     except KeyboardInterrupt:
         loop.run_until_complete(restore_channel())
         loop.run_until_complete(client.close())
